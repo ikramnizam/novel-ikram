590f6805953894b79d3f593bbd03a00599ecb04b
ikramnizam <ikrmnzm@gmai.com>
2024-10-08 13:10:43 +0800

    adjust style and anable mustverifyemail

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,11 +10,13 @@
     <!-- FontAwesome for icons -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
 </head>
+
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
             <a class="navbar-brand" href="#">Novel Manager ikramsss</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -26,18 +29,23 @@
                             <a class="nav-link" href="{{ route('register') }}">Register</a>
                         </li>
                     @else
-                    <li class="nav-item">
-                        <form action="{{ route('novels.index') }}" method="GET" class="d-inline">
-                            @csrf
-                            <button type="submit" class="btn btn-link nav-link">Novels</button>
-                        </form>
-                    </li>
-                        <li class="nav-item">
-                            <form action="{{ route('logout') }}" method="POST" class="d-inline">
-                                @csrf
-                                <button type="submit" class="btn btn-link nav-link">Logout</button>
-                            </form>
-                        </li>
+                        <div class="d-flex align-items-center ">
+                            <li class="nav-item text-white ">
+                                Welcome, {{ auth()->user()->name }}
+                            </li>
+                            {{-- <li class="nav-item">
+                                <form action="{{ route('novels.index') }}" method="GET" class="d-inline">
+                                    @csrf
+                                    <button type="submit" class="btn btn-link nav-link">Novels</button>
+                                </form>
+                            </li> --}}
+                            <li class="nav-item">
+                                <form action="{{ route('logout') }}" method="POST" class="d-inline">
+                                    @csrf
+                                    <button type="submit" class="btn btn-link nav-link text-white">Logout</button>
+                                </form>
+                            </li>
+                        </div>
                     @endguest
                 </ul>
             </div>
@@ -53,4 +61,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
+
+{{-- Welcome, {{ auth()->user()->name }} --}}
